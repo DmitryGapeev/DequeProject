35b507bed7d2bae7ecefeb278c0e9870612ac481
Дмитрий Гапеев <gapeevdirog@gmail.com>
2019-01-17 23:16:03 +0300

    Add project files.

---
+++ DequeProject/Deque.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+  class Deque<T>
+  {
+    private LinkedList<T> _items;
+    public Deque()
+    {
+      _items = new LinkedList<T>();
+    }
+
+    public void AddFront(T item)
+    {
+      _items.AddFirst(new LinkedListNode<T>(item));
+    }
+
+    public void AddTail(T item)
+    {
+      _items.AddLast(item);
+    }
+
+    public T RemoveFront()
+    {
+      if(_items.Count == 0)
+        return default(T);
+
+      T result = _items.First.Value;
+      _items.RemoveFirst();
+      return result;
+    }
+
+    public T RemoveTail()
+    {
+      if(_items.Count == 0)
+        return default(T);
+
+      T result = _items.Last.Value;
+      _items.RemoveLast();
+      return result;
+    }
+
+    public int Size()
+    {
+      return _items.Count;
+    }
+  }
+
+}
---
+++ DequeProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string string1 = "hello world";
+      string string2 = "спел лепс";
+
+      Console.WriteLine(string1 + " is palindrom = " + IsPalindrome(string1));
+      Console.WriteLine(string2 + " is palindrom = " + IsPalindrome(string2));
+
+      Console.ReadKey();
+    }
+
+    static bool IsPalindrome(string str)
+    {
+      Deque<char> deque = new Deque<char>();
+      bool isPalindrome = true;
+
+      foreach (var symblol in str)
+        deque.AddFront(symblol);
+
+      foreach (var symblol in str)
+        deque.AddFront(symblol);
+
+      while (deque.Size() != 0 && isPalindrome)
+      {
+        char symblolFromFront = deque.RemoveFront();
+        char symblolFromTail = deque.RemoveTail();
+
+        isPalindrome = symblolFromFront == symblolFromTail;
+      }
+
+      return isPalindrome;
+    }
+  }
+}
