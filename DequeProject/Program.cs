93956a2a59e2889d2ba9a544a4560a2c68297c57
Гапеев Дмитрий Игоревич <Dmitriy.Gapeev@galaktika.by>
2019-01-18 09:16:08 +0300

    task 4

--- DequeProject/Program.cs
+++ DequeProject/Program.cs
@@ -10,6 +10,8 @@ namespace AlgorithmsDataStructures
   {
     static void Main(string[] args)
     {
+			TestDeque();
+
       string string1 = "hello world";
       string string2 = "спел лепс";
 
@@ -24,10 +26,10 @@ namespace AlgorithmsDataStructures
       Deque<char> deque = new Deque<char>();
       bool isPalindrome = true;
 
-      foreach (var symblol in str)
+      foreach (char symblol in str)
         deque.AddFront(symblol);
 
-      foreach (var symblol in str)
+      foreach (char symblol in str)
         deque.AddFront(symblol);
 
       while (deque.Size() != 0 && isPalindrome)
@@ -40,5 +42,69 @@ namespace AlgorithmsDataStructures
 
       return isPalindrome;
     }
+
+
+	  static void TestDeque()
+	  {
+		  Console.WriteLine("test deque");
+		  Deque<int> deque = new Deque<int>();
+
+		  int i = 1;
+
+		  Console.WriteLine("add in front");
+		  for (; i <= 10; i++)
+		  {
+			  deque.AddFront(i);
+			  Console.Write(i + " ");
+		  }
+
+		  Console.WriteLine();
+		  Console.WriteLine("size after add in front = " + deque.Size());
+		  Console.WriteLine();
+
+		  for (; i <= 20; i++)
+		  {
+			  deque.AddTail(i);
+			  Console.Write(i + " ");
+		  }
+
+		  Console.WriteLine();
+		  Console.WriteLine("size after add in tail = " + deque.Size());
+		  Console.WriteLine();
+
+			Console.WriteLine("remove from front");
+		  for (int j = 0; j < 5; j++)
+			  Console.Write(deque.RemoveFront()+" ");
+
+		  Console.WriteLine();
+		  Console.WriteLine("size after remove from front = " + deque.Size());
+		  Console.WriteLine();
+
+		  Console.WriteLine("remove from tail");
+		  for (int j = 0; j < 5; j++)
+			  Console.Write(deque.RemoveTail() + " ");
+
+		  Console.WriteLine();
+		  Console.WriteLine("size after remove from tail = " + deque.Size());
+		  Console.WriteLine();
+
+			Console.WriteLine("remote from front");
+		  while (deque.Size() != 0)
+			  Console.Write(deque.RemoveFront() + " ");
+
+		  Console.WriteLine();
+		  Console.WriteLine("size after remove from tail = " + deque.Size());
+		  Console.WriteLine();
+
+			Console.WriteLine("try to remove from tail, deque size = " + deque.Size());
+		  int result = deque.RemoveTail();
+			Console.WriteLine("result = " + result);
+
+		  Console.WriteLine("try to remove from front, deque size = " + deque.Size());
+		  result = deque.RemoveFront();
+		  Console.WriteLine("result = " + result);
+
+			Console.WriteLine(new string('=', 50));
+	  }
   }
 }
